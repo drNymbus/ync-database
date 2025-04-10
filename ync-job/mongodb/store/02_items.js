19e9b25a7a6d8aea54c8f3fb353831db1faacce4
nymbus <xxvmelanconxx@gmail.com>
2025-04-10 15:12:06 +0200

    Added bin image in the cheapest way. Actually no need for GridFS for store keyspace (every file is <16Mb).

--- ync-job/mongodb/store/02_items.js
+++ ync-job/mongodb/store/02_items.js
@@ -1,19 +1,23 @@
 use store;
 
+let data = undefined;
+
+data = cat('assets/Quelconque.png', {encoding: 'base64'});
 db.item.insertOne({
     id: "dummy",
     category: "None",
-    image: BinData(0, "some-bin-string"),  // MongoDB uses Binary for blobs
+    image: BinData(0, "some-bin-dummy-string"),  // MongoDB uses Binary for blobs
     display_name: "Test",
     description: "Lorem ipsum dolor sit amet, consectetur adip",
     basket_description: "This should not be in a basket, how did you come so far?",
     price: 10.99  // MongoDB supports decimal for precise currency storage
 });
 
+data = cat('assets/Quelconque.png', {encoding: 'base64'});
 db.item.insertOne({
     id: "quelconque",
     category: "quelconque",
-    image: BinData(0, "some-bin-string"), // "store/assets/Quelconque.png"
+    image: BinData(0, data),
     display_name: "Quelconque",
     description: "“Quelconque” est une photo prise au lac de Gradignan le 14 mars 2024. Alexandre et Victor jouent aux échecs, Alexis prend des photos. Le soleil se couche, il a déjà commencé à se cacher.",
     basket_description: "Livré avec son cadre photo aléatoire.",
